bfdf363898a547faa3c3e163e326087f4dd3a292
saish0 <patrickvalera50@yahoo.com>
2022-04-06 15:32:00 -0700

    first commmit | boiler plate

---
+++ vue_timer/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
+import Timer from './components/Timer.vue'
+</script>
+
+<template>
+  <Header/>
+  <main>
+    <Timer/>
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+</style>
---
+++ vue_timer/src/components/Timer.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  data() {
+    return {
+      timerEnabled: 0,
+      time: 0,
+      minutes: 0,
+      secs: 0,
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      console.log(this.timerEnabled);
+      this.timerEnabled = true;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="form" v-if="!timerEnabled">
+    <form v-if="!timerEnabled" @submit.prevent="handleSubmit">
+      <input v-model="minutes" type="number" placeholder="m" />
+      <input v-model="secs" type="number" placeholder="s" />
+      <input type="submit" value="submit" />
+    </form>
+  </div>
+  <div v-else>
+    <span> {{ minutes }}:{{ secs }} </span>
+  </div>
+</template>
+
+<style></style>
